79ffbfc4214618bbe18f68b05f33675a90d0ef33
Hannah Lappe <hannah@lappe.page>
2022-02-17 20:04:30 +0100

    changes to workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,59 @@
 plugins {
-    kotlin("multiplatform") version "1.6.10"
-    id("maven-publish")
+  kotlin("multiplatform") version "1.6.10"
+  id("maven-publish")
 }
 
 group = "page.lappe"
 version = "0.1"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  val nativeTarget = when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
 
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
+  nativeTarget.apply {
+    binaries {
+      executable {
+        entryPoint = "main"
+      }
     }
-    sourceSets {
-        val nativeMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
-            }
-        }
-        val nativeTest by getting
+  }
+  sourceSets {
+    val nativeMain by getting {
+      dependencies {
+        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
+      }
     }
+    val nativeTest by getting
+  }
 }
 
 
 
 publishing {
-    publications {
-        create<MavenPublication>("kotlin-nativ-std") {
-
-        }
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/HanLap/kotlin-nativ-std")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
     }
+  }
 
-    repositories {
-        maven {
-            name = "github"
-            url = uri("https://maven.pkg.github.com/HanLap/kotlin-nativ-std")
-        }
+  publications {
+    register<MavenPublication>("gpr") {
+      from(components["kotlin"])
     }
+  }
 }
